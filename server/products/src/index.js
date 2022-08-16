e600e7eafe1a70d87146edcf737c4585b7b18fe3
Chitwan Bindal <chitwan001@gmail.com>
2022-08-16 11:56:41 +0530

    updated

--- server/products/src/index.js
+++ server/products/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const indexRoute = require('./routes/index.routes');
+const cors = require('cors');
+
+app.use(cors({ origin: '*' }));
+
 app.use('/',indexRoute)
+
 
 app.listen(3002,()=>{
     console.log("products service started on 3002")
--- server/products/src/routes/index.routes.js
+++ server/products/src/routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.json({'status':'products Connected!'})
+    res.json({'status':'Products Connected!'})
 })
 
 module.exports = router;
